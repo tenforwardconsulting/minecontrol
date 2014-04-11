4b31b7e2a5fc46d4cfd8d189597872d71084f3bd
Brian Samson <brian@briansamson.com>
2014-04-11 15:05:02 -0500

    configure for heroku

--- server.rb
+++ server.rb
@@ -1,12 +1,36 @@
 require 'sinatra'
 require 'aws'
 
-config = YAML::load_file(File.join(File.dirname(__FILE__), 'config.yml'))
-AWS.config(config['aws'])
-puts "Starting up with configuration: "
+config_file = File.join(File.dirname(__FILE__), 'config.yml')
+if File.exist? config_file
+  config = YAML::load_file(config_file)
+  puts "Starting up with configuration from file"
+else
+  config = {
+    'basic_auth': {
+      'username': ENV['MC_BASIC_AUTH_USERNAME'],
+      'password': ENV['MC_BASIC_AUTH_PASSWORD']
+    },
+    'server': {
+      'instance_id': ENV['MC_SERVER_INSTANCE_ID']
+    },
+    'aws': {
+      'access_key_id': ENV['MC_AWS_ACCESS_KEY_ID'],
+      'secret_access_key': ENV['MC_AWS_SECRET_ACCESS_KEY'],
+      'region': ENV['MC_AWS_REGION']
+    }
+  }
+  put "using config from environment"
+end
 
-use Rack::Auth::Basic, "MineControl" do |username, password|
-  username == config['basic_auth']['username'] and password == config['basic_auth']['password']
+raise "Bad config" unless config['server'] && config['aws']
+
+AWS.config(config['aws'])
+
+if (config['basic_auth'])
+  use Rack::Auth::Basic, "MineControl" do |username, password|
+    username == config['basic_auth']['username'] and password == config['basic_auth']['password']
+  end
 end
 
 get '/' do
